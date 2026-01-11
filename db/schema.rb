c5fdc1bf0619d4f66e91fcee94bd2faee35abfaf
Robson Peacock <robson_peacock20@outlook.com>
2026-01-11 15:43:35 +0000

    Add Project model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_05_214812) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_09_170739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "status"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
 
   create_table "solid_cable_messages", force: :cascade do |t|
     t.binary "channel", null: false
