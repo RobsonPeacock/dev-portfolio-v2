433f93276186de04e51ebce0a89aebeb37075086
Robson Peacock <robson_peacock20@outlook.com>
2026-01-12 17:40:25 +0000

    Configure root API endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def index
+    render json: {
+      status: "online",
+      version: "v1",
+      message: "Welcome to my portfolio"
+    }
+  end
 end
