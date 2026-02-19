72ed4deff5de1089d73738323754d34f49ea11ef
Robson Peacock <robson_peacock20@outlook.com>
2026-02-19 23:02:32 +0000

    Rename SSM parameters to follow production naming conventions

--- terraform/live/secrets.tf
+++ terraform/live/secrets.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "db_name" {
-  name = "/prod/backend/dev-portfolio-db-name"
+  name = "/prod/backend/dev_portfolio_db_name"
   type = "SecureString"
   value = "change_me"
 
@@ -9,7 +9,7 @@ resource "aws_ssm_parameter" "db_name" {
 }
 
 resource "aws_ssm_parameter" "db_username" {
-  name = "/prod/backend/dev-portfolio-db-username"
+  name = "/prod/backend/dev_portfolio_db_username"
   type = "SecureString"
   value = "change_me"
 
@@ -19,7 +19,7 @@ resource "aws_ssm_parameter" "db_username" {
 }
 
 resource "aws_ssm_parameter" "db_password" {
-  name = "/prod/backend/dev-portfolio-db-password"
+  name = "/prod/backend/dev_portfolio_db_password"
   type = "SecureString"
   value = "change_me"
 
