32e5e93dc75334c27a3b14d3f90b2bb521fa0067
Robson Peacock <robson_peacock20@outlook.com>
2026-05-11 23:32:23 +0100

    Add project section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import HeroSection from '../components/hero_section.jsx';
 import SkillsSection from '../components/skills_section.jsx';
 import GithubCalendar from '../components/github_calendar.jsx';
 import WorkExperienceSection from '../components/work_experience_section.jsx';
+import ProjectSection from '../components/project_section.jsx';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <SkillsSection />
           <GithubCalendar />
           <WorkExperienceSection />
+          <ProjectSection />
         </main>
       </div>
     </>
