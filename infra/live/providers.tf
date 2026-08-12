0621501b958c6f8be732cd94ac6a52301a6a86a9
Robson Peacock <robson_peacock20@outlook.com>
2026-08-12 01:11:26 +0100

    Setup Cloudflare provider

--- infra/live/providers.tf
+++ infra/live/providers.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.28.0"
     }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.23.0"
+    }
   }
 
   required_version = "~> 1.14.0"
@@ -12,3 +17,5 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+provider "cloudflare" {}
