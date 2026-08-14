7983a5fd8654f30ef2fe97d9eea92a4b31d0a7cd
Robson Peacock <robson_peacock20@outlook.com>
2026-08-15 00:38:48 +0100

    Add DNS records

--- infra/live/cloudflare.tf
+++ infra/live/cloudflare.tf
@@ -3,3 +3,21 @@ data "cloudflare_zone" "dev_portfolio_zone" {
     name = "robsonpeacock.dev"
   }
 }
+
+resource "cloudflare_dns_record" "dev_portfolio_apex_record" {
+  zone_id = data.cloudflare_zone.dev_portfolio_zone.id
+  name    = "@"
+  content = aws_cloudfront_distribution.react_frontend_distribution.domain_name
+  type    = "CNAME"
+  proxied = false
+  ttl     = 300
+}
+
+resource "cloudflare_dns_record" "dev_portfolio_www_record" {
+  zone_id = data.cloudflare_zone.dev_portfolio_zone.id
+  name    = "www"
+  content = data.cloudflare_zone.dev_portfolio_zone.filter.name
+  type    = "CNAME"
+  proxied = false
+  ttl     = 300
+}
