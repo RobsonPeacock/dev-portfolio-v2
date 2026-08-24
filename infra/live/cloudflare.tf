6ed286119aa6c4d8c69e0ffd5f882dd0a877d7c7
Robson Peacock <robson_peacock20@outlook.com>
2026-08-25 00:09:19 +0100

    Add Cloudflare email routing DNS records

--- infra/live/cloudflare.tf
+++ infra/live/cloudflare.tf
@@ -21,3 +21,33 @@ resource "cloudflare_dns_record" "dev_portfolio_www_record" {
   proxied = false
   ttl     = 300
 }
+
+resource "cloudflare_email_routing_dns" "dev_portfolio_email_dns" {
+  zone_id = data.cloudflare_zone.dev_portfolio_zone.id
+}
+
+resource "cloudflare_email_routing_address" "dev_portfolio_email_address" {
+  account_id  = var.cloudflare_account_id
+  email   = var.destination_email
+}
+
+resource "cloudflare_email_routing_rule" "dev_portfolio_email_rule" {
+  zone_id = data.cloudflare_zone.dev_portfolio_zone.id
+  name = "Forward contact address"
+  enabled = true
+
+  matchers = [
+    {
+      type = "literal"
+      field = "to"
+      value = var.custom_email
+    }
+  ]
+
+  actions = [
+    {
+      type = "forward"
+      value = [var.destination_email]
+    }
+  ]
+}
--- infra/live/variables.tf
+++ infra/live/variables.tf
@@ -13,3 +13,18 @@ variable "enable_maint_mode" {
   type    = bool
   default = false
 }
+
+variable "cloudflare_account_id" {
+  description = "The Cloudflare account ID"
+  type        = string
+}
+
+variable "custom_email" {
+  description = "The email address to use for Cloudflare email routing"
+  type        = string
+}
+
+variable "destination_email" {
+  description = "The destination email address to forward emails to"
+  type        = string
+}
